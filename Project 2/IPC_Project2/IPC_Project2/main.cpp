fb18b9d1cb16892ca2e0709fed3b52693b3563f0
Darklight437 <darklight437@gmail.com>
2017-06-21 10:26:29 +1000

    2nd process complete

--- Project 2/IPC_Project2/IPC_Project2/main.cpp
+++ Project 2/IPC_Project2/IPC_Project2/main.cpp
@@ -105,3 +105,120 @@ void releaseSharedMemory()
         g_hFileMapping = NULL;
     }
 }
+
+bool keyIsDown(int code)
+{
+    return (GetAsyncKeyState(code) & 0x8000) != 0;
+}
+
+void gameLoop_mutex()
+{
+    std::cout << "======================\n";
+    std::cout << "Mutex demo - Process 2\n";
+    std::cout << "======================\n\n";
+    std::cout << "Usage:\n";
+    std::cout << "    - Press '0' to own mutex\n";
+    std::cout << "    - Press ESC to quit\n\n";
+
+    std::cout << "Press any key to start game loop...\n\n";
+    _getch();
+
+    bool owned = false;
+    while (true)
+    {
+        std::cout << (owned ? "owned" : "not owned") << "\n";
+
+        // Acquire ownership of the mutex as long as the '0' key is held down
+        if (!owned && keyIsDown('0'))
+        {
+            owned = getMutexOwnership();
+        }
+        if (owned && !keyIsDown('0'))
+        {
+            releaseMutexOwnership();
+            owned = false;
+        }
+
+        Sleep(100);
+
+        // Quit on ESCAPE
+        if (keyIsDown(VK_ESCAPE))
+        {
+            break;
+
+        }
+
+    } // while
+}
+
+void gameLoop_sharedMemory()
+{
+    std::cout << "==============================\n";
+    std::cout << "Shared Memory demo - Process 2\n";
+    std::cout << "==============================\n\n";
+    std::cout << "Usage:\n";
+    std::cout << "    - Press ESC to quit\n\n";
+
+    std::cout << "Press any key to start game loop...\n\n";
+    _getch();
+
+    int frameCount = 0;
+    bool owned = false;
+    while (true)
+    {
+        // Attempt to read shared memory and output to cout
+        if (getMutexOwnership())
+        {
+            std::cout << frameCount++ << "  " << (char *)g_pSharedMemory << "\n";
+            releaseMutexOwnership();
+        }
+        else
+        {
+            std::cout << frameCount++ << "\n";
+        }
+
+        Sleep(50);
+
+        // Quit on ESCAPE
+        if (keyIsDown(VK_ESCAPE))
+        {
+            break;
+
+        }
+
+    } // while
+}
+
+//==============================================================================///
+//                                                                              ///
+//==============================================================================///
+
+int main()
+{
+
+    try
+    {
+        // Create a global mutex
+        createMutex();
+
+        // Create a global block of shared memory
+        createSharedMemory("Foo Memory", 10000);
+
+
+        gameLoop_mutex(); // Game loop to demo mutex handling
+                          //gameLoop_sharedMemory(); // Game loop to demo shared memory
+
+                          // Cleanup
+        releaseSharedMemory();
+        destroyMutex();
+
+        std::cout << "\nFinished! Press any key...\n\n";
+        _getch();
+    }
+    catch (const char *e)
+    {
+        std::cout << "EXCEPTION - " << e << "\n\n";
+    }
+
+    return 0;
+}
